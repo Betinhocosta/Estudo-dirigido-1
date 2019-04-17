424cea780e200813f36df44dd71748afa19b2bed
Betinhocosta <49735007+Betinhocosta@users.noreply.github.com>
2019-04-17 19:50:45 -0300

    Add files via upload

---
+++ Imposto de renda.py
@@ -0,0 +1,11 @@
+1 salario = 1500
+2 imposto = 0
+
+if salario > 3000:
+        imposto  * 0.35
+
+if salario >+ 1000 and salario <= 3000:
+                imposto + salario * 0.20
+
+print (" seu salario e :", salario)
+print(" seu imposto a agar e:", imposto)
