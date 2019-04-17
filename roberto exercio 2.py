9c2befe5ee612d5cb6e2031ef298f5a6922847f4
Betinhocosta <49735007+Betinhocosta@users.noreply.github.com>
2019-04-17 19:50:26 -0300

    Add files via upload

---
+++ roberto exercio 2.py
@@ -0,0 +1,16 @@
+n1 = 8
+n2 = 9
+n3 = 5
+n4 = 8
+
+media = ( n1 + n2 +n3 + n4)/4
+
+if media >= 7:
+    print("aprovado")
+
+if media >=5 and media < 7:
+    print("exame")
+
+if media < 5:
+    print('reprovado')
+
